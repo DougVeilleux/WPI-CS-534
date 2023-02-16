48cd8feecb89d7e1739050a4881bf2f18fe289dc
austinaguirre <theaguirrefamily@gmail.com>
2023-02-15 21:05:18 -0500

    added the ASTAR to the romania city app and added a gitignore

--- GroupProject01/RomaniaCityApp.py
+++ GroupProject01/RomaniaCityApp.py
@@ -1,15 +1,21 @@
 import json
+import os
 
-from GroupAssignment1 import romania_map_data
-from GroupAssignment1.SimpleProblemSolvingAgent import SimpleProblemSolvingAgent
+import romania_map_data
+import SimpleProblemSolvingAgent
 
 
 def main():
-    print("Welcome to the RomaniaCityApp.")
+    print('=====================================')
+    print("Welcome to the RomaniaCityApp.\n")
 
     data = getMapData()
     map_data = romania_map_data.UndirectedGraph(data["distances"])
     locations = data["locations"]
+
+    print("List of Cities:")
+    print(map_data.nodes())
+    print()
 
     again = True
     while again:
@@ -17,20 +23,28 @@ def main():
 
         start_city = twocities[0]
         end_city = twocities[1]
-        search_strategy = "bfs"
 
-        problem = SimpleProblemSolvingAgent(map_data, locations)
-        route = problem.search(start_city, end_city, search_strategy)
+        problem = SimpleProblemSolvingAgent.SimpleProblemSolvingAgent(map_data, locations)
+        routeBFS = problem.search(start_city, end_city, 'BFS')
+        routeASTAR = problem.search(start_city, end_city, 'ASTAR')
 
-        if route is not None:
-            print("Route found: ", route)
+        if routeBFS is not None:
+            print("Route found using BFS: ", routeBFS)
         else:
-            print("Route not found.")
+            print("Route not found using BFS.")
+
+        if routeASTAR is not None:
+            (came_from, cost_so_far) = routeASTAR
+            routeASTAR = problem.reconstruct_path(came_from, start_city, end_city)
+            print("Route found using ASTAR: ", routeASTAR)
+        else:
+            print("Route not found using ASTAR.")
 
         again = input("Again? Yes/No ")
         if again.lower() == "yes":
             again = True
         elif again.lower() == "no":
+            print("Thank you for using the RomaniaCityApp!")
             again = False
 
 
@@ -38,7 +52,12 @@ def getMapData():
     notfound = True
     while notfound:
         print("Where is the map file in your local directory?")
+        print("Example Syntax (windows): C:\\Users\\User\\Documents\\mapdata.txt")
+        print("Example Syntax (mac): /Users/...Project01/mapdata.txt")
         path = input()
+        # path = "/Users/dougveilleux/Documents/GitHub/CS-534/WPI-CS-534/GroupProject01/mapdata.txt"
+        # cwd = os.getcwd()
+        # path = os.path.join(cwd, "mapdata.txt")
         try:
             with open(path) as f:
                 data = f.read()
@@ -48,7 +67,7 @@ def getMapData():
         else:
             notfound = False
 
-    print("Map file found!")
+    print("Map file found!\n")
 
     return json.loads(data)
 
@@ -64,7 +83,7 @@ def inputCities(locations):
         if city1 in locations and city2 in locations:
             again = False
 
-    print("Cities are valid searching for best path!")
+    print("\nCities are valid...searching for best path!")
     return cities
 
 
