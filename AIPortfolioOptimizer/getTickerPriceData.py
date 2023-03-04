a5b5dff46b19be741eb41c23d10b24f96f214500
Doug Veilleux <110361876+DougVeilleux@users.noreply.github.com>
2023-03-03 23:48:20 -0500

    directory added

---
+++ AIPortfolioOptimizer/getTickerPriceData.py
@@ -0,0 +1,91 @@
+"""
+getTickerPriceData.py
+
+Script to pull Ticker Symbol Data from specified website.
+"""
+
+"""
+==============================================================================
+Import Modules
+------------------------------------------------------------------------------
+"""
+import datetime as dt
+import numpy as np
+import pandas_datareader.data as pdr
+import yfinance as yf
+
+from getTickerSymbolsHTML import dup_delete, get_tickers
+
+
+"""
+==============================================================================
+Functions
+------------------------------------------------------------------------------
+"""
+def get_price_data(ticker_symbols, start_date, end_date):
+    """
+    Function uses a yahoo finance API call to pull price data:
+        open, high, low, close, adj close, volume
+        for the ticker_symbols specified
+    :param ticker_symbols: list format
+    :param start_date: datetime format
+    :param end_date: datetime format
+    :return: price in dataframe format
+    """
+
+    # --- Make API call for price data ---
+    try:
+        print('INFO: API Call for Price Data.')
+        yf.pdr_override()  # <== that's all it takes :-) This is needed for wb.get_data_yahoo())
+        data = pdr.get_data_yahoo(ticker_symbols, start_date, end_date)
+        data = data.replace(np.nan, 0)
+        print('INFO: API Call Success.  Price Data Acquired.')
+        return data
+
+    except:
+        print('ERROR: >>> Could not get price data from API call.')
+
+
+"""
+==============================================================================
+Scripting
+------------------------------------------------------------------------------
+"""
+# --- Specify desired index: 'SP500', 'DOW30', or 'NASDAQ'
+index = 'DOW30'
+
+# --- Get ticker symbols
+tickers = dup_delete(get_tickers(index))
+print(50* '=')
+print('INFO: Getting Ticker Symbols...')
+print(tickers)
+print('INFO: Complete.')
+print(50* '^')
+
+
+# --- Specify desired time period.
+start_date = dt.datetime(2022,12,1)        #Year:Month:Day
+end_date   = dt.datetime.today()
+
+
+# --- Get Price Data
+print()
+print(50* '=')
+print('INFO: Pulling Price Data For Following Dates.')
+print(f" start date = {start_date}, end date = {end_date}")
+df = get_price_data(tickers,start_date,end_date)
+print('INFO: Complete.')
+print(50* '^')
+
+# ---  Write to .csv file
+print()
+print(50* '=')
+print('INFO: Writing Price Data to .csv...')
+#  Reformat dates for .csv filename
+start_date = start_date.strftime("%d %b, %Y")
+end_date = end_date.strftime("%d %b, %Y")
+base_filename = 'PriceData'
+df.to_csv(base_filename + ' From ' + start_date + ' To ' + end_date + '.csv')
+print('Filename: ' + base_filename + ' From ' + start_date + ' To ' + end_date + '.csv')
+print('INFO: Complete.')
+print(50* '^')
--- AIPortfolioOptimizer/getTickerSymbolsHTML.py
+++ AIPortfolioOptimizer/getTickerSymbolsHTML.py
@@ -3,7 +3,16 @@ getTickerSymbolsHTML.py
 
 Script to pull Ticker Symbol Data from specified website.
 """
+
+
+"""
+==============================================================================
+Import Modules
+------------------------------------------------------------------------------
+"""
 import pandas as pd
+
+
 
 
 """
@@ -35,6 +44,19 @@ def get_tickers(index):
         tickers = [i.replace('.', '-') for i in tickers]
         return tickers
 
+    if(index.upper() == 'DOW30'):
+        # Get DOW 30 Tickers
+        table = pd.read_html('https://en.wikipedia.org/wiki/Dow_Jones_Industrial_Average')[1]
+        table = table.sort_values('Symbol')
+        tickers = table.Symbol.to_list()
+        tickers = [i.replace('.', '-') for i in tickers]
+        return tickers
 
-tickers = get_tickers('SP500')
-print(tickers)
+    if (index.upper() == 'DOW30'):
+        # Get NASDAQ 100 Tickers
+        table = pd.read_html('https://en.wikipedia.org/wiki/NASDAQ-100')[4]
+        table = table.sort_values('Ticker')
+        tickers = table.Ticker.to_list()
+        tickers = [i.replace('.', '-') for i in tickers]
+        return tickers
+
