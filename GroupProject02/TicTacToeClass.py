d5f10717907238086e2fe3b1a85303d7597ede2e
Doug Veilleux <110361876+DougVeilleux@users.noreply.github.com>
2023-03-06 22:16:48 -0500

    copied https://github.com/aimacode/aima-python code and started into 2 files

--- GroupProject02/TicTacToeClass.py
+++ GroupProject02/TicTacToeClass.py
@@ -1,5 +1,5 @@
-
-
+# ====================================================================================================================
+# --- Define Imports:
 from collections import namedtuple, Counter, defaultdict
 import random
 import math
@@ -8,31 +8,37 @@ cache = functools.lru_cache(10**6)
 
 
 
-
+# ====================================================================================================================
 # --- Define Classes:
 class Game:
-    """A game is similar to a problem, but it has a terminal test instead of
-    a goal test, and a utility for each terminal state. To create a game,
-    subclass this class and implement `actions`, `result`, `is_terminal`,
-    and `utility`. You will also need to set the .initial attribute to the
-    initial state; this can be done in the constructor."""
-
+    """A game is similar to a problem, but it has a terminal test instead of a goal
+    test, and a utility for each terminal state.  To create a game, subclass this class a
+    nd implement 'actions', 'result', 'is_terminal', and 'utility'.  You will also need to
+    set the .initial attribute to the initial state; this can be done in the constructor.
+    """
     def actions(self, state):
-        """Return a collection of the allowable moves from this state."""
+        """
+        Return a collection of the allowable moves from this state.
+        """
         raise NotImplementedError
 
     def result(self, state, move):
-        """Return the state that results from making a move from a state."""
-        raise NotImplementedError
+        """
+        Return the state that results from making a move from a state.
+        """
+        raise ModuleNotFoundError
 
     def is_terminal(self, state):
-        """Return True if this is a final state for the game."""
+        """
+        Return True if this is a final state for the game.
+        """
         return not self.actions(state)
 
     def utility(self, state, player):
-        """Return the value of this final state to player."""
+        """
+        Return the value of this final state to player.
+        """
         raise NotImplementedError
-
 class TicTacToe(Game):
     """Play TicTacToe on an `height` by `width` board, needing `k` in a row to win.
     'X' plays first against 'O'."""
@@ -63,72 +69,11 @@ class TicTacToe(Game):
         return board.utility != 0 or len(self.squares) == len(board)
 
     def display(self, board): print(board)
-
-    infinity = math.inf
-    def minimax_search(game, state):
-        """Search game tree to determine best move; return (value, move) pair."""
-
-        player = state.to_move
-
-        def max_value(state):
-            if game.is_terminal(state):
-                return game.utility(state, player), None
-            v, move = -infinity, None
-            for a in game.actions(state):
-                v2, _ = min_value(game.result(state, a))
-                if v2 > v:
-                    v, move = v2, a
-            return v, move
-
-        def min_value(state):
-            if game.is_terminal(state):
-                return game.utility(state, player), None
-            v, move = +infinity, None
-            for a in game.actions(state):
-                v2, _ = max_value(game.result(state, a))
-                if v2 < v:
-                    v, move = v2, a
-            return v, move
-
-        return max_value(state)
-
-    def alphabeta_search(game, state):
-        """Search game to determine best action; use alpha-beta pruning.
-        As in [Figure 5.7], this version searches all the way to the leaves."""
-
-        player = state.to_move
-
-        def max_value(state, alpha, beta):
-            if game.is_terminal(state):
-                return game.utility(state, player), None
-            v, move = -infinity, None
-            for a in game.actions(state):
-                v2, _ = min_value(game.result(state, a), alpha, beta)
-                if v2 > v:
-                    v, move = v2, a
-                    alpha = max(alpha, v)
-                if v >= beta:
-                    return v, move
-            return v, move
-
-        def min_value(state, alpha, beta):
-            if game.is_terminal(state):
-                return game.utility(state, player), None
-            v, move = +infinity, None
-            for a in game.actions(state):
-                v2, _ = max_value(game.result(state, a), alpha, beta)
-                if v2 < v:
-                    v, move = v2, a
-                    beta = min(beta, v)
-                if v <= alpha:
-                    return v, move
-            return v, move
-
-        return max_value(state, -infinity, +infinity)
-
 class Board(defaultdict):
-    """A board has the player to move, a cached utility value,
-    and a dict of {(x, y): player} entries, where player is 'X' or 'O'."""
+    """
+    A board has the player to move, a cached utility value, and a dict of
+    {(x,y): player} entries, where player is 'X' or 'O'.
+    """
     empty = '.'
     off = '#'
 
@@ -157,10 +102,23 @@ class Board(defaultdict):
 
         return '\n'.join(map(row, range(self.height))) + '\n'
 
-# --- Functions:
+
+
+# ====================================================================================================================
+# --- Define Functions:
+def k_in_row(board, player, square, k):
+    """
+    True if player has k pieces in a line through square.
+    """
+    def in_row(x, y, dx, dy): return 0 if board[x, y] != player else 1 + in_row(x + dx, y + dy, dx, dy)
+    return any(in_row(*square, dx, dy) + in_row(*square, -dx, -dy) - 1 >= k
+               for (dx, dy) in ((0, 1), (1, 0), (1, 1), (1, -1)))
+
 def play_game(game, strategies: dict, verbose=False):
-    """Play a turn-taking game. `strategies` is a {player_name: function} dict,
-    where function(state, game) is used to get the player's move."""
+    """
+    Play a turn-taking game. `strategies` is a {player_name: function} dict,
+    where function(state, game) is used to get the player's move.
+    """
     state = game.initial
     while not game.is_terminal(state):
         player = state.to_move
@@ -170,11 +128,81 @@ def play_game(game, strategies: dict, verbose=False):
             print('Player', player, 'move:', move)
             print(state)
     return state
-def k_in_row(board, player, square, k):
-    """True if player has k pieces in a line through square."""
-    def in_row(x, y, dx, dy): return 0 if board[x, y] != player else 1 + in_row(x + dx, y + dy, dx, dy)
-    return any(in_row(*square, dx, dy) + in_row(*square, -dx, -dy) - 1 >= k
-               for (dx, dy) in ((0, 1), (1, 0), (1, 1), (1, -1)))
+
+def minimax_search(game, state):
+    """
+    Search game tree to determine best move; return (value, move) pair.
+    """
+    player = state.to_move
+
+    def max_value(state):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        v, move = -infinity, None
+        for a in game.actions(state):
+            v2, _ = min_value(game.result(state, a))
+            if v2 > v:
+                v, move = v2, a
+        return v, move
+
+    def min_value(state):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        v, move = +infinity, None
+        for a in game.actions(state):
+            v2, _ = max_value(game.result(state, a))
+            if v2 < v:
+                v, move = v2, a
+        return v, move
+
+    return max_value(state)
+
+infinity = math.inf
+
+def alphabeta_search(game, state):
+    """
+    Search game to determine best action; use alpha-beta pruning.
+    As in [Figure 5.7], this version searches all the way to the leaves.
+    """
+
+    player = state.to_move
+
+    def max_value(state, alpha, beta):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        v, move = -infinity, None
+        for a in game.actions(state):
+            v2, _ = min_value(game.result(state, a), alpha, beta)
+            if v2 > v:
+                v, move = v2, a
+                alpha = max(alpha, v)
+            if v >= beta:
+                return v, move
+        return v, move
+
+    def min_value(state, alpha, beta):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        v, move = +infinity, None
+        for a in game.actions(state):
+            v2, _ = max_value(game.result(state, a), alpha, beta)
+            if v2 < v:
+                v, move = v2, a
+                beta = min(beta, v)
+            if v <= alpha:
+                return v, move
+        return v, move
+
+    return max_value(state, -infinity, +infinity)
+
+def random_player(game, state): return random.choice(list(game.actions(state)))
+
+def player(search_algorithm):
+    """
+    A game player who uses the specified search algorithm
+    """
+    return lambda game, state: search_algorithm(game, state)[1]
+
 
 
 
--- GroupProject02/TicTacToeGameApp.py
+++ GroupProject02/TicTacToeGameApp.py
@@ -1,19 +1,12 @@
+# ====================================================================================================================
+# --- Define Imports:
+from TicTacToeClass import TicTacToe, player, play_game, random_player, alphabeta_search, minimax_search
 
-from collections import namedtuple, Counter, defaultdict
-import random
-import math
-import functools
-cache = functools.lru_cache(10**6)
-
-# --- Define Functions:
-def random_player(game, state): return random.choice(list(game.actions(state)))
-
-def player(search_algorithm):
-    """A game player who uses the specified search algorithm"""
-    return lambda game, state: search_algorithm(game, state)[1]
 
 
 # Play the game:
+def main():
+    play_game(TicTacToe(), dict(X=random_player, O=player(alphabeta_search)), verbose=True).utility
 
-play_game(TicTacToe(), dict(X=random_player, O=player(alphabeta_search)), verbose=True).utility
-
+if __name__ == '__main__':
+    main()
