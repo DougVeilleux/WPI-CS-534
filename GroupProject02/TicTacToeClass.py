f6e6f147856e78f5de891f141322993d1e18881f
Doug Veilleux <110361876+DougVeilleux@users.noreply.github.com>
2023-03-11 15:43:33 -0500

    copied Austins work into the code base to get merge to work

--- GroupProject02/TicTacToeClass.py
+++ GroupProject02/TicTacToeClass.py
@@ -1,208 +1,133 @@
 # ====================================================================================================================
 # --- Define Imports:
-from collections import namedtuple, Counter, defaultdict
+
+from typing import List
+from copy import deepcopy
+from random import choice
+from Game import Game
+from Players import Player
 import random
-import math
-import functools
-cache = functools.lru_cache(10**6)
 
 
 
 # ====================================================================================================================
 # --- Define Classes:
-class Game:
-    """A game is similar to a problem, but it has a terminal test instead of a goal
-    test, and a utility for each terminal state.  To create a game, subclass this class a
-    nd implement 'actions', 'result', 'is_terminal', and 'utility'.  You will also need to
-    set the .initial attribute to the initial state; this can be done in the constructor.
-    """
-    def actions(self, state):
-        """
-        Return a collection of the allowable moves from this state.
-        """
-        raise NotImplementedError
-
-    def result(self, state, move):
-        """
-        Return the state that results from making a move from a state.
-        """
-        raise ModuleNotFoundError
-
-    def is_terminal(self, state):
-        """
-        Return True if this is a final state for the game.
-        """
-        return not self.actions(state)
-
-    def utility(self, state, player):
-        """
-        Return the value of this final state to player.
-        """
-        raise NotImplementedError
 class TicTacToe(Game):
-    """Play TicTacToe on an `height` by `width` board, needing `k` in a row to win.
-    'X' plays first against 'O'."""
-
-    def __init__(self, height=3, width=3, k=3):
-        self.k = k # k in a row
-        self.squares = {(x, y) for x in range(width) for y in range(height)}
-        self.initial = Board(height=height, width=width, to_move='X', utility=0)
-
-    def actions(self, board):
-        """Legal moves are any square not yet taken."""
-        return self.squares - set(board)
-
-    def result(self, board, square):
-        """Place a marker for current player on square."""
-        player = board.to_move
-        board = board.new({square: player}, to_move=('O' if player == 'X' else 'X'))
-        win = k_in_row(board, player, square, self.k)
-        board.utility = (0 if not win else +1 if player == 'X' else -1)
-        return board
-
-    def utility(self, board, player):
-        """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
-        return board.utility if player == 'X' else -board.utility
-
-    def is_terminal(self, board):
-        """A board is a terminal state if it is won or there are no empty squares."""
-        return board.utility != 0 or len(self.squares) == len(board)
-
-    def display(self, board): print(board)
-class Board(defaultdict):
     """
-    A board has the player to move, a cached utility value, and a dict of
-    {(x,y): player} entries, where player is 'X' or 'O'.
+    Tic Tac Toe game class.
     """
-    empty = '.'
-    off = '#'
 
-    def __init__(self, width=8, height=8, to_move=None, **kwds):
-        self.__dict__.update(width=width, height=height, to_move=to_move, **kwds)
+    def __init__(self, size=3):
+        self.size = size
+        self.moves = [(i, j) for i in range(size) for j in range(size)]
+        self.players = ('X', 'O')
+        self.winning_combinations = (
+                [[(i, j) for i in range(size)] for j in range(size)]  # rows
+                + [[(i, j) for j in range(size)] for i in range(size)]  # columns
+                + [[(i, i) for i in range(size)]]  # diagonal
+                + [[(i, size - i - 1) for i in range(size)]]  # anti-diagonal
+        )
+        self.initial = [[' ' for _ in range(size)] for _ in range(size)]
 
-    def new(self, changes: dict, **kwds) -> 'Board':
-        "Given a dict of {(x, y): contents} changes, return a new Board with the changes."
-        board = Board(width=self.width, height=self.height, **kwds)
-        board.update(self)
-        board.update(changes)
-        return board
+    def actions(self, state: List[List[str]]) -> List:
+        return [(i, j) for i, row in enumerate(state) for j, val in enumerate(row) if val == ' ']
 
-    def __missing__(self, loc):
-        x, y = loc
-        if 0 <= x < self.width and 0 <= y < self.height:
-            return self.empty
+    def result(self, state: List[List[str]], move: tuple, player: str) -> List[List[str]]:
+        state_copy = deepcopy(state)
+        state_copy[move[0]][move[1]] = player
+        return state_copy
+
+    def winner(self, state: List[List[str]]) -> str:
+        for player in self.players:
+            for combination in self.winning_combinations:
+                if all(state[i][j] == player for i, j in combination):
+                    return player
+        return None
+
+    def terminal_test(self, state: List[List[str]]) -> bool:
+        return self.winner(state) is not None or all(val != ' ' for row in state for val in row)
+
+    def utility(self, state: List[List[str]], player: str) -> int:
+        if self.winner(state) == player:
+            return 1
+        elif self.winner(state) == self.players[(self.players.index(player) + 1) % 2]:
+            return -1
         else:
-            return self.off
+            return 0
 
-    def __hash__(self):
-        return hash(tuple(sorted(self.items()))) + hash(self.to_move)
+    def display(self, state: List[List[str]]) -> None:
+        for i in range(self.size):
+            print(' | '.join(state[i]))
+            if i < self.size - 1:
+                print('---' * self.size)
+        print()
 
-    def __repr__(self):
-        def row(y): return ' '.join(self[x, y] for x in range(self.width))
+    def to_move(self, state: List[List[str]]) -> str:
+        num_X = sum(row.count('X') for row in state)
+        num_O = sum(row.count('O') for row in state)
+        if num_X == num_O:
+            return 'X'
+        else:
+            return 'O'
 
-        return '\n'.join(map(row, range(self.height))) + '\n'
 
-
-
-# ====================================================================================================================
-# --- Define Functions:
-def k_in_row(board, player, square, k):
+class Human(Player):
     """
-    True if player has k pieces in a line through square.
+    Human player class.
     """
-    def in_row(x, y, dx, dy): return 0 if board[x, y] != player else 1 + in_row(x + dx, y + dy, dx, dy)
-    return any(in_row(*square, dx, dy) + in_row(*square, -dx, -dy) - 1 >= k
-               for (dx, dy) in ((0, 1), (1, 0), (1, 1), (1, -1)))
 
-def play_game(game, strategies: dict, verbose=False):
-    """
-    Play a turn-taking game. `strategies` is a {player_name: function} dict,
-    where function(state, game) is used to get the player's move.
-    """
-    state = game.initial
-    while not game.is_terminal(state):
-        player = state.to_move
-        move = strategies[player](game, state)
-        state = game.result(state, move)
-        if verbose:
-            print('Player', player, 'move:', move)
-            print(state)
-    return state
+    def move(self, game: Game, state: List[List[str]]) -> tuple:
+        print(f"Possible moves: {game.actions(state)}")
+        move = input("Enter your move (i,j): ")
+        return tuple(map(int, move.split(',')))
 
-def minimax_search(game, state):
-    """
-    Search game tree to determine best move; return (value, move) pair.
-    """
-    player = state.to_move
 
-    def max_value(state):
-        if game.is_terminal(state):
+class AI(Player):
+    """
+    AI player class.
+    """
+
+    def __init__(self, name: str, algorithm: str):
+        super().__init__(name)
+        self.algorithm = algorithm
+
+    # def move(self, game: Game, state: List[List[str]]) -> tuple:
+    #     _, move = self.minimax(game, state) if self.algorithm == 'minimax' else self.alphabeta(game, state)
+    #     return move
+    def move(self, game: Game, state: List[List[str]]) -> tuple:
+        if self.algorithm == 'minimax':
+            _, move = self.minimax(game, state)
+        elif self.algorithm == 'alphabeta':
+            _, move = self.alphabeta(game, state)
+        elif self.algorithm == 'random':
+            move = self.random_player(game, state)
+        return move
+
+    def minimax(self, game: Game, state: List[List[str]], depth=1) -> tuple:
+        player = game.to_move(state)
+        if game.terminal_test(state):
             return game.utility(state, player), None
-        v, move = -infinity, None
-        for a in game.actions(state):
-            v2, _ = min_value(game.result(state, a))
-            if v2 > v:
-                v, move = v2, a
-        return v, move
+        scores = []
+        for action in game.actions(state):
+            new_state = game.result(state, action, player)
+            score, _ = self.minimax(game, new_state, depth + 1)
+            scores.append((-score, action))
+        return max(scores)
 
-    def min_value(state):
-        if game.is_terminal(state):
+    def alphabeta(self, game: Game, state: List[List[str]], depth=1, alpha=float('-inf'), beta=float('inf')) -> tuple:
+        player = game.to_move(state)
+        if game.terminal_test(state):
             return game.utility(state, player), None
-        v, move = +infinity, None
-        for a in game.actions(state):
-            v2, _ = max_value(game.result(state, a))
-            if v2 < v:
-                v, move = v2, a
-        return v, move
+        best_score, best_action = float('-inf'), None
+        for action in game.actions(state):
+            new_state = game.result(state, action, player)
+            score, _ = self.alphabeta(game, new_state, depth + 1, -beta, -max(alpha, best_score))
+            if -score > best_score:
+                best_score, best_action = -score, action
+            if best_score >= beta:
+                break
+        return best_score, best_action
 
-    return max_value(state)
-
-infinity = math.inf
-
-def alphabeta_search(game, state):
-    """
-    Search game to determine best action; use alpha-beta pruning.
-    As in [Figure 5.7], this version searches all the way to the leaves.
-    """
-
-    player = state.to_move
-
-    def max_value(state, alpha, beta):
-        if game.is_terminal(state):
-            return game.utility(state, player), None
-        v, move = -infinity, None
-        for a in game.actions(state):
-            v2, _ = min_value(game.result(state, a), alpha, beta)
-            if v2 > v:
-                v, move = v2, a
-                alpha = max(alpha, v)
-            if v >= beta:
-                return v, move
-        return v, move
-
-    def min_value(state, alpha, beta):
-        if game.is_terminal(state):
-            return game.utility(state, player), None
-        v, move = +infinity, None
-        for a in game.actions(state):
-            v2, _ = max_value(game.result(state, a), alpha, beta)
-            if v2 < v:
-                v, move = v2, a
-                beta = min(beta, v)
-            if v <= alpha:
-                return v, move
-        return v, move
-
-    return max_value(state, -infinity, +infinity)
-
-def random_player(game, state): return random.choice(list(game.actions(state)))
-
-def player(search_algorithm):
-    """
-    A game player who uses the specified search algorithm
-    """
-    return lambda game, state: search_algorithm(game, state)[1]
-
-
-
-
+    def random_player(self, game: Game, state):
+        """A player that chooses a legal move at random."""
+        return random.choice(game.actions(state)) if game.actions(state) else None
